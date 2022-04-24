191224dcf5320cf9b1fe9ed33df970261f4ada8f
Lucas <lucasquin@protonmail.com>
2022-04-24 16:32:39 -0300

    Exploring functions

--- Learning/functions/src/main.rs
+++ Learning/functions/src/main.rs
@@ -4,13 +4,30 @@
 
 #[allow(unused_variables)]
 #[allow(unused_assignments)]
+#[allow(dead_code)]
 
 fn main() {
+
     let name: &str = "Lucas";
+    let mut surname: &str = "";
+    let hi = say_hi(name);
+    
     say_hello(name);
-    println!("{}", name);
+    say_surname(name, &mut surname);
+    println!("{}", hi);
 }
 
 fn say_hello(name: &str){
     println!("Hello, {} ", name);
+}
+
+fn say_surname(name: &str, surname: &mut &str){
+    
+    *surname = "Lopes";
+    println!("{} {}", name, surname);
+}
+
+fn say_hi(name: &str) -> String {
+    let hi: String = format!("Hi, {}", name);
+    return hi;
 }
